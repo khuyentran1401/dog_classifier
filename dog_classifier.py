3f4c2ee6aabce31e3b7793eb8a8b55255533f96b
khuyentran1401 <khuyentran1476@gmail.com>
2021-01-30 22:07:59 -0600

    init

---
+++ dog_classifier.py
@@ -0,0 +1,45 @@
+import fastbook
+fastbook.setup_book()
+
+from fastbook import *
+from fastai.vision.widgets import *
+from pathlib import Path
+
+import streamlit as st
+
+class Predict:
+    def __init__(self, filename):
+        self.learn_inference = load_learner(filename)
+        self.img = self.get_image_from_upload()
+        if self.img is not None:
+            self.display_output()
+            self.get_prediction()
+    
+    @staticmethod
+    def get_image_from_upload():
+        uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+        if uploaded_file is not None:
+            return PILImage.create((uploaded_file))
+        return None
+
+    def display_output(self):
+        st.image(self.img.to_thumb(500,500), caption='Uploaded Image')
+
+    def get_prediction(self):
+
+        if st.button('Classify'):
+            pred, pred_idx, probs = self.learn_inference.predict(self.img)
+            st.write(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
+        else: 
+            st.write(f'Click the button to classify') 
+
+if __name__=='__main__':
+    path = Path('')
+    file_name='dog.pkl'
+
+    filename=path/file_name
+
+    predictor = Predict(filename)
+    # predictor.get_prediction()
+
+    
