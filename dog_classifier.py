c9c3dd22fb1d27ad60b73d847caab7b9789ea854
khuyentran1401 <khuyentran1476@gmail.com>
2021-02-01 09:45:04 -0600

    fix requirements.txt

--- dog_classifier.py
+++ dog_classifier.py
@@ -2,16 +2,20 @@
 # fastbook.setup_book()
 
 # from fastbook import *
-from fastai.vision import *
-from fastai import *
+from fastai.vision.widgets import *
+from fastai.vision.all import *
 
 from pathlib import Path
+import pickle
+import os 
 
 import streamlit as st
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
+
 class Predict:
     def __init__(self, filename):
-        self.learn_inference = load_learner(filename)
+        self.learn_inference = load_learner(Path(ROOT_DIR)/filename)
         self.img = self.get_image_from_upload()
         if self.img is not None:
             self.display_output()
@@ -36,12 +40,14 @@ class Predict:
             st.write(f'Click the button to classify') 
 
 if __name__=='__main__':
-    path = Path('')
+    path = Path()
     file_name='dog.pkl'
 
-    filename=path/file_name
+    # load_learner(path/file_name)
 
-    predictor = Predict(filename)
-    # predictor.get_prediction()
+    # filename=path/file_name
+    # print(filename)
+
+    predictor = Predict(file_name)
 
     
